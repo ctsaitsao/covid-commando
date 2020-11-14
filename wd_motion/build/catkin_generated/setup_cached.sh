20d97b5e5f707e0daafab1522ca7ec1ed95cc75f
wu-di995 <diwu2020@u.northwestern.edu>
2020-11-14 14:14:10 -0600

    Added wd_motion

---
+++ wd_motion/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/skrdown/ME495/final-project-covid_commando/wd_motion/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/skrdown/ME495/final-project-covid_commando/wd_motion/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/skrdown/ME495/final-project-covid_commando/wd_motion/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/skrdown/ME495/final-project-covid_commando/wd_motion/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/skrdown/ME495/final-project-covid_commando/wd_motion/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/skrdown/ME495/final-project-covid_commando/wd_motion/src:$ROS_PACKAGE_PATH"
---
+++ wd_motion/build/motion/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "baxter_interface;rospy".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "motion"
+PROJECT_SPACE_DIR = "/home/skrdown/ME495/final-project-covid_commando/wd_motion/devel"
+PROJECT_VERSION = "0.0.0"
